040a327d1b0ac5ad97ea6124b4d2adf0d10c4346
Noah Boeck <naboe05@gmail.com>
2026-06-12 14:00:14 +0200

    first version

---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "6rem",
+          marginBottom: "1rem",
+          letterSpacing: "0.1em",
+        }}
+      >
+        KARL ARSCH
+      </h1>
+
+      <p>UNDER CONSTRUCTION</p>
+
+      <p
+        style={{
+          marginTop: "3rem",
+          opacity: 0.5,
+        }}
+      >
+      </p>
+    </main>
+  );
+}
